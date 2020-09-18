c6abb355933fbabf8c1b06b95b8e930e83217df0
Alexandru Tudose <alex.tudose@tapptitude.com>
2020-09-18 17:06:48 +0300

    Initial Commit

---
+++ prana-bluetooth/prana_bluetoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  prana_bluetoothApp.swift
+//  prana-bluetooth
+//
+//  Created by Alexandru Tudose on 18.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct prana_bluetoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
